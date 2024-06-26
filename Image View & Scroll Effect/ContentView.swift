0bb543fe5875024ba12e80442c2b21cb81b303c5
Afeez <harfyzz@gmail.com>
2024-06-26 10:21:19 +0100

    Initial Commit

---
+++ Image View & Scroll Effect/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Image View & Scroll Effect
+//
+//  Created by Afeez Yunus on 26/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
