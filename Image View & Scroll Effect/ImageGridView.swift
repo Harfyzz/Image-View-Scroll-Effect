d1fe4d9d8f4d09b7c18568cf7532c7eb0b7c851b
Afeez <harfyzz@gmail.com>
2024-06-26 15:59:41 +0100

    -added a gallery view

---
+++ Image View & Scroll Effect/ImageGridView.swift
@@ -0,0 +1,18 @@
+//
+//  ImageGridView.swift
+//  Image View & Scroll Effect
+//
+//  Created by Afeez Yunus on 26/06/2024.
+//
+
+import SwiftUI
+
+struct ImageGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageGridView()
+}
